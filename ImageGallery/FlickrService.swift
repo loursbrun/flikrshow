29e79dd697b4a24fad70297135c14618e1ccd6cb
Fabien Brun <fabienbrun@hotmail.fr>
2017-03-24 15:31:42 +0100

    Parse Son to object

--- ImageGallery/FlickrService.swift
+++ ImageGallery/FlickrService.swift
@@ -14,7 +14,7 @@
 /* ----------------------------------------- */
 
 /*     https://www.flickr.com/services/api/explore/flickr.photos.search    */
-
+/*                                                                         */
 /*     https://www.flickr.com/services/api/misc.urls.html                  */
 
 
@@ -25,32 +25,48 @@ import Foundation
 class FlickrService {
     static let sharedInstance: FlickrService = {
         
-        /* -----------    URL Flickr     ------------ */
-        let helloURL = URL(string: "https://api.flickr.com/services/rest/?method=flickr.photos.search&api_key=3a8ae2ce4481865b5e7de17a2fbce311&tags=sport&format=json&nojsoncallback=1&api_sig=8fac08b8e7be735a09f96c95f365b88f")!
-        /* ----------------------------------------- */
-
         
         let instance = FlickrService()
         
-        // setup code
-        print("Hello FlickrService !!! ")
+        
         
         let session = URLSession.shared
         
-       
         
-        let task = session.dataTask(with: helloURL){
+        
+        let usersGetURL = URL(string: "https://api.flickr.com/services/rest/?method=flickr.photos.search&api_key=3a8ae2ce4481865b5e7de17a2fbce311&tags=sport&format=json&nojsoncallback=1&api_sig=8fac08b8e7be735a09f96c95f365b88f")!
+        
+        var requestUsers = URLRequest(url: usersGetURL)
+        requestUsers.httpMethod = "GET"
+        
+        
+        let getUsersTask = session.dataTask(with: usersGetURL){
             (data, response, error) in
             guard error == nil else {
                 print(error!)
                 return
             }
-            print(data!)
-            print(String(data: data!, encoding: String.Encoding.utf8)!)
+            guard let data = data else {
+                print("missing data")
+                return
+            }
+            
+            let json = try? JSONSerialization.jsonObject(with: data, options: [])
+            
+            if let photos = json {
+                print(photos)
+            }
+            
+            
+            
+            
+            
+            
+            //print(data!)
+            //print(String(data: data, encoding: String.Encoding.utf8)!)
         }
         
-        task.resume()
-        
+        getUsersTask.resume()
         
     
         
